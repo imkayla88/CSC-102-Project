929f56e34107a2858f90da26c7a03fc93be4a165
Kayla <105653549+imkayla88@users.noreply.github.com>
2026-04-01 11:07:22 -0400

    Update bomb_phases.py added names

--- bomb_phases.py
+++ bomb_phases.py
@@ -1,7 +1,7 @@
 #################################
 # CSC 102 Defuse the Bomb Project
 # GUI and Phase class definitions
-# Team: 
+# Team: Cyron, Kayla, and Nabil
 #################################
 
 # import the configs
