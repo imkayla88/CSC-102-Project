2dfa6da7d08dedd68f1783403e938f99cd0fad55
Kayla <105653549+imkayla88@users.noreply.github.com>
2026-04-01 11:02:14 -0400

    Update bomb_configs.py - added names and cypher words

--- bomb_configs.py
+++ bomb_configs.py
@@ -1,12 +1,13 @@
 #################################
 # CSC 102 Defuse the Bomb Project
 # Configuration file
-# Team: 
+# Team: Cyron, Kayla, and Nabil
 #################################
 
 # constants
 DEBUG = False        # debug mode?
-RPi = False           # is this running on the RPi?
+RPi = True           # is this running on the RPi?
+ANIMATE = True       # animate the LCD text?
 SHOW_BUTTONS = False # show the Pause and Quit buttons on the main LCD GUI?
 COUNTDOWN = 300      # the initial bomb countdown value (seconds)
 NUM_STRIKES = 5      # the total strikes allowed before the bomb "explodes"
@@ -73,7 +74,7 @@ if (RPi):
         pin.value = True
 
 # toggle switches
-# 3x3 pins: 12, 16, 20, 21, 3V3, 3V3, 3V3, 3V3, GND, GND, GND, GND
+# 3x3 pins: 12, 16, 20, 21, 3V3, 3V3, 3V3, 3V3
 #           -TOG1-  -TOG2-  --TOG3--  --TOG4--  --TOG5--  --TOG6--
 if (RPi):
     # the pins
@@ -84,52 +85,113 @@ if (RPi):
         pin.pull = Pull.DOWN
 
 ###########
-# functions to generate targets for toggles/wires/keypad/Button
+# functions
 ###########
+# generates the bomb's serial number
 def genSerial():
-    # Create your own logic of making a serial number (if needed)
-    # TODO
-    return "B026DES"
+    # set the digits (used in the toggle switches phase)
+    serial_digits = []
+    toggle_value = randint(1, 15)
+    # the sum of the digits is the toggle value
+    while (len(serial_digits) < 3 or toggle_value - sum(serial_digits) > 0):
+        d = randint(0, min(9, toggle_value - sum(serial_digits)))
+        serial_digits.append(d)
 
-def genTogglesTarget():
-    # Create your own logic of making a target number for toggles
-    # TODO
-    return 20
+    # set the letters (used in the jumper wires phase)
+    jumper_indexes = [ 0 ] * 5
+    while (sum(jumper_indexes) < 3):
+        jumper_indexes[randint(0, len(jumper_indexes) - 1)] = 1
+    jumper_value = int("".join([ str(n) for n in jumper_indexes ]), 2)
+    # the letters indicate which jumper wires must be "cut"
+    jumper_letters = [ chr(i + 65) for i, n in enumerate(jumper_indexes) if n == 1 ]
 
-def genWiresTarget():
-    # Create your own logic of making a target number for wires
-    # TODO
-    return 5
+    # form the serial number
+    serial = [ str(d) for d in serial_digits ] + jumper_letters
+    # and shuffle it
+    shuffle(serial)
+    # finally, add a final letter (F..Z)
+    serial += [ choice([ chr(n) for n in range(70, 91) ]) ]
+    # and make the serial number a string
+    serial = "".join(serial)
+
+    return serial, toggle_value, jumper_value
+
 # generates the keypad combination from a keyword and rotation key
-def genKeypadTarget():
-    # Create your own logic of making a keypad combination number if needed
-    # TODO
-    return "26863"
+def genKeypadCombination():
+    # encrypts a keyword using a rotation cipher
+    def encrypt(keyword, rot):
+        cipher = ""
+        # encrypt each letter of the keyword using rot
+        for c in keyword:
+            cipher += chr((ord(c) - 65 + rot) % 26 + 65)
+        return cipher
+
+    # returns the keypad digits that correspond to the passphrase
+    def digits(passphrase):
+        combination = ""
+        keys = [ None, None, "ABC", "DEF", "GHI", "JKL", "MNO", "PRS", "TUV", "WXY" ]
+        # process each character of the keyword
+        for c in passphrase:
+            for i, k in enumerate(keys):
+                if (k and c in k):
+                    # map each character to its digit equivalent
+                    combination += str(i)
+        return combination
+
+    # the list of keywords and matching passphrases
+    keywords = { "BANDIT": "RIVER",\
+                 "BUCKLE": "FADED",\
+                 "CANOPY": "FOXES",\
+                 "DEBATE": "THROW",\
+                 "FIERCE": "TRICK",\
+                 "GIFTED": "CYCLE",\
+                 "IMPACT": "STOLE",\
+                 "LONELY": "TOADY",\
+                 "MIGHTY": "ALOOF",\
+                 "NATURE": "CARVE",\
+                 "REBORN": "CLIMB",\
+                 "RECALL": "FEIGN",\
+                 "SYSTEM": "LEAVE",\
+                 "TAKING": "SPINY",\
+                 "WIDELY": "BOUND",\
+                 "ZAGGED": "YACHT" }
+    # the rotation cipher key
+    rot = randint(1, 25)
+
+    # pick a keyword and matching passphrase
+    keyword, passphrase = choice(list(keywords.items()))
+    # encrypt the passphrase and get its combination
+    cipher_keyword = encrypt(keyword, rot)
+    combination = digits(passphrase)
+
+    return keyword, cipher_keyword, rot, combination, passphrase
+
+###############################
+# generate the bomb's specifics
+###############################
+# generate the bomb's serial number (which also gets us the toggle and jumper target values)
+serial, toggles_target, wires_target = genSerial()
+
+# generate the combination for the keypad phase
+keyword, cipher_keyword, rot, keypad_target, passphrase = genKeypadCombination()
 
 # generate the color of the pushbutton (which determines how to defuse the phase)
 button_color = choice(["R", "G", "B"])
+# appropriately set the target (R is None)
+button_target = None
+# G is the first numeric digit in the serial number
+if (button_color == "G"):
+    button_target = [ n for n in serial if n.isdigit() ][0]
+# B is the last numeric digit in the serial number
+elif (button_color == "B"):
+    button_target = [ n for n in serial if n.isdigit() ][-1]
 
-def genButtonTarget():
-    # TODO
-    global button_color
-    # Create your own logic of making a Button target
-    # appropriately set the target (R is None)
-    b_target = None
-    # G is the first numeric digit in the serial number
-    if (button_color == "G"):
-        b_target = [ n for n in serial if n.isdigit() ][0]
-    # B is the last numeric digit in the serial number
-    elif (button_color == "B"):
-        b_target = [ n for n in serial if n.isdigit() ][-1]
-
-    return b_target
-
-###############################
-serial = genSerial()
-toggles_target = genTogglesTarget()
-wires_target = genWiresTarget()
-keypad_target = genKeypadTarget()
-button_target = genButtonTarget()
+if (DEBUG):
+    print(f"Serial number: {serial}")
+    print(f"Toggles target: {bin(toggles_target)[2:].zfill(4)}/{toggles_target}")
+    print(f"Wires target: {bin(wires_target)[2:].zfill(5)}/{wires_target}")
+    print(f"Keypad target: {keypad_target}/{passphrase}/{keyword}/{cipher_keyword}(rot={rot})")
+    print(f"Button target: {button_target}")
 
 # set the bomb's LCD bootup text
 boot_text = f"Booting...\n\x00\x00"\
@@ -142,4 +204,3 @@ boot_text = f"Booting...\n\x00\x00"\
             f"*{' '.join(ascii_uppercase)}\n"\
             f"*{' '.join([str(n % 10) for n in range(26)])}\n"\
             f"Rendering phases...\x00"
-            
