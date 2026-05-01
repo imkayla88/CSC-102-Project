26c9b284e0052807f348551de00a1478f13a517e
NabilSabbooba <nabeel.saboubeh@gmail.com>
2026-05-01 11:24:54 -0400

    Add title label for bomb defusal instructions

--- bomb_phases2.py
+++ bomb_phases2.py
@@ -43,9 +43,11 @@ class Lcd(Frame):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=2)
         self.columnconfigure(2, weight=1)
+        self._title = Label(self, bg="black", fg="red", font=("Courier New", 30, "bold"), text="DEFUSE THE BOMB")
+        self._title.grid(row=0, column=0, columnspan=3, pady=(30, 10))
         # the scrolling informative "boot" text
         self._lscroll = Label(self, bg="black", fg="white", font=("Courier New", 14), text="", justify=LEFT)
-        self._lscroll.grid(row=0, column=0, columnspan=3, sticky=W)
+        self._lscroll.grid(row=1, column=0, columnspan=3, sticky=W)
         self.pack(fill=BOTH, expand=True)
         
         # kill switch button (always visible)
