8132d8dbca469f06602d259f7ed21e5f631ecba2
NabilSabbooba <nabeel.saboubeh@gmail.com>
2026-05-01 10:31:04 -0400

    Remove character display from bomb_configs.py
    Removed display of uppercase letters and numbers in bomb configuration.

--- bomb_configs.py
+++ bomb_configs.py
@@ -201,6 +201,4 @@ boot_text = f"Booting...\n\x00\x00"\
             f"*Serial number: {serial}\n"\
             f"Encrypting keypad...\n\x00"\
             f"*Keyword: {cipher_keyword}; key: {rot}\n"\
-            f"*{' '.join(ascii_uppercase)}\n"\
-            f"*{' '.join([str(n % 10) for n in range(26)])}\n"\
             f"Rendering phases...\x00"
