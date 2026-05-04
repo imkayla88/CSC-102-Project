ca14a90bfd78fdf32f98a46689b891bef9e6cabe
NabilSabbooba <nabeel.saboubeh@gmail.com>
2026-05-04 09:08:49 -0400

    Adjust title label grid settings
    Removed padding from the title label grid configuration.

--- bomb_phases2.py
+++ bomb_phases2.py
@@ -56,7 +56,7 @@ class Lcd(Frame):
         self.columnconfigure(1, weight=2)
         self.columnconfigure(2, weight=1)
         self._title = Label(self, bg="black", fg="#00ff00", font=("Courier New", 30, "bold"), text="DEFUSE THE BOMB")
-        self._title.grid(row=0, column=0, columnspan=3, sticky=W, padx=40)
+        self._title.grid(row=0, column=0, columnspan=3)
         # the scrolling informative "boot" text
         self._lscroll = Label(self, bg="black", fg="#00ff00", font=("Courier New", 14), text="", justify=LEFT)
         self._lscroll.grid(row=1, column=0, columnspan=3, sticky=W, padx=40)
